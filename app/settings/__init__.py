abdbf12f6f4f22d11110774a347dbaf2ffa4a905
Matias Sotelo <matias.sotee@gmail.com>
2020-07-26 19:35:59 -0300

    Settings: creating files for each environment

---
+++ app/settings/__init__.py
@@ -0,0 +1,10 @@
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
